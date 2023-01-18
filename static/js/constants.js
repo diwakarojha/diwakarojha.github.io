58de60c1232897a9ccb0c0aa9a4bb77468bc797b
diwakarojha <dojha00@gmail.com>
2023-01-18 15:55:28 +0530

    added chat bot for Shardeen

---
+++ static/js/constants.js
@@ -0,0 +1,5 @@
+const action_name = "/greet";
+// Replace with Public IP address where Rasa server is hosted below
+// const rasa_server_url = "https://9228-2405-201-600e-b0b2-afd9-5f0e-aa92-5d35.in.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const sender_id = uuidv4();
